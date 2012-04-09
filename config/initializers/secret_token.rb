776eba1a76463422f353398a17a2b7e82ad87790
Dan Koscielak <dano@koscielak.com>
2012-04-09 09:18:52 -0400

    my project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InxiteData::Application.config.secret_token = '3ca527dcec01e30ac2a36fa8290154c1416b06bd44ca15977d0deba3af7981b7d818518d0146d3c5aacccca0291676738df30170da7b10b01804bcd5837a5ef4'
